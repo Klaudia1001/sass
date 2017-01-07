d7aa58d2316c6f65ed29a5fdf48483992582773f
Klaudia Przespolewska <klaudia1000@onet.eu>
2017-01-07 11:44:48 +0100

    zmiany

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp=require('gulp'); //tworzy środowisko gulpa
 var sass=require('gulp-sass');
 
 
-//na obiekcie gulpa wykonuje funkcje. Ma skompilowac pliki
-gulp.task('sass', function  
+//na obiekcie gulpa wykonuje konkretna funkcje. Ma skompilowac pliki do sassa
+gulp.task('sass', function (){
     return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'));
 });
 
-//funkcja spowoduje, że gulp będzie obserwować czy nastąpiły zmiany w pliku
+//funkcja spowoduje, że gulp będzie obserwować czy nastąpiły zmiany w pliku, jeśli zmieniłam to sam to przekompiluje i wykona powyższą funkcję
 gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.scss'['sass']);
+    gulp.watch('app/scss/**/*.scss',['sass']);
 });
